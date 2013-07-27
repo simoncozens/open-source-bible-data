ac7a9d158f289ab04bd88cf2e640ef4a853e4e7d
Simon Cozens <simon@simon-cozens.org>
2013-07-27 16:28:21 +0900

    More files I had lying around.

--- catalogue.js
+++ catalogue.js
@@ -2,7 +2,7 @@ Catalogue = [
 	{
 		type: "bible",
 		version: { abbrev: "KJV", full: "King James Version" },
-		lang: "en",
+		lang: "eng",
 		licence: "Public Domain",
 		source: "http://ebible.org/kjv/",
 		files: {
@@ -14,7 +14,7 @@ Catalogue = [
 	{
 		type: "bible",
 		version: { abbrev: "LEB", full: "Lexham English Bible" },
-		lang: "en",
+		lang: "eng",
 		licence: "Custom",
 		licence_url: "http://lexhamenglishbible.com/license/",
 		source: "http://lexhamenglishbible.com/",
@@ -57,6 +57,46 @@ Catalogue = [
 			raw: { type: "remote", url: "https://github.com/openscriptures/morphhb/tree/master/wlc", format: "OSIS" },
 			simpleXml: "cooked/simple-xml/wlc.xml",
 			sqlite: "cooked/sqlite/wlc.db"
+		}
+	},
+	{
+		type: "bible",
+		version: { abbrev: "POR-Almeida", full: "Tradução de João Ferreira de Almeida" },
+		lang: "por",
+		licence: "Public Domain",
+		files: {
+			simpleXml: "cooked/simple-xml/por.xml",
+			sqlite: "cooked/sqlite/por.db"
+		}
+	},
+	{
+		type: "bible",
+		version: { abbrev: "ReinaV", full: "Reina Valera Actualizada" },
+		lang: "spa",
+		licence: "Public Domain(?)",
+		files: {
+			simpleXml: "cooked/simple-xml/rva.xml",
+			sqlite: "cooked/sqlite/rva.db"
+		}
+	},	
+	{
+		type: "bible",
+		version: { abbrev: "ReinaV", full: "Reina Valera 1909" },
+		lang: "spa",
+		licence: "Public Domain",
+		files: {
+			simpleXml: "cooked/simple-xml/rvr.xml",
+			sqlite: "cooked/sqlite/rvr.db"
+		}
+	},	
+	{
+		type: "bible",
+		version: { abbrev: "SE", full: "Sagradas Escrituras" },
+		lang: "spa",
+		licence: "Public Domain",
+		files: {
+			simpleXml: "cooked/simple-xml/se.xml",
+			sqlite: "cooked/sqlite/se.db"
 		}
 	},	
 ];
